0375ddbf70a283c61475d362d2e769561065d8f3
pkhao75_Legion <132234282+pkhao75@users.noreply.github.com>
2024-07-22 17:02:00 -0400

    Update Model methods

--- src/main/java/group5/model/Model.java
+++ src/main/java/group5/model/Model.java
@@ -1,5 +1,27 @@
 package group5.model;
 
+import java.util.List;
+
+import group5.model.beans.MBeans;
+
 public class Model implements IModel {
+
+    @Override
+    public List<MBeans> loadSourceData() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadSourceData'");
+    }
+
+    @Override
+    public List<MBeans> loadWatchList(String filename) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadWatchList'");
+    }
+
+    @Override
+    public void saveWatchList(String filename, List<MBeans> watchList) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveWatchList'");
+    }
 
 }
