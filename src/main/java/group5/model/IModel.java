ee1299fa5067b5371428401e315158a81e916ab4
pkhao75_Legion <132234282+pkhao75@users.noreply.github.com>
2024-07-22 18:52:28 -0400

    Add Model Interface methods

--- src/main/java/group5/model/IModel.java
+++ src/main/java/group5/model/IModel.java
@@ -2,17 +2,26 @@ package group5.model;
 
 import group5.model.beans.MBeans;
 
+import java.util.Collection;
+import java.util.stream.Stream;
 import java.util.List;
 
 public interface IModel {
 
     public static final String DEFAULT_DATA = "";  // Path to default movie DB here
 
-    List<MBeans> loadSourceData();
+    void loadSourceData();
 
-    List<MBeans> loadWatchList(String filename);
+    void loadWatchList();
 
-    void saveWatchList(String filename, List<MBeans> watchList);
+    Stream<MBeans> getSourceLists();
+
+    Stream<MBeans> getWatchLists(String filename);
+
+    void saveWatchList(String filename, Stream<MBeans> watchList);
+
+    Stream<MBeans> getFiltered();
+
 
 
 }
--- src/main/java/group5/model/Model.java
+++ src/main/java/group5/model/Model.java
@@ -1,27 +1,48 @@
 package group5.model;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import group5.model.beans.MBeans;
 
 public class Model implements IModel {
 
     @Override
-    public List<MBeans> loadSourceData() {
+    public void loadSourceData() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'loadSourceData'");
     }
 
     @Override
-    public List<MBeans> loadWatchList(String filename) {
+    public void loadWatchList() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'loadWatchList'");
     }
 
     @Override
-    public void saveWatchList(String filename, List<MBeans> watchList) {
+    public Stream<MBeans> getSourceLists() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSourceLists'");
+    }
+
+    @Override
+    public Stream<MBeans> getWatchLists(String filename) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWatchLists'");
+    }
+
+    @Override
+    public void saveWatchList(String filename, Stream<MBeans> watchList) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'saveWatchList'");
     }
+
+    @Override
+    public Stream<MBeans> getFiltered() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFiltered'");
+    }
+
+
 
 }
