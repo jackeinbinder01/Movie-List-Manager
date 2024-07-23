fbe41afc46c0bfa82de5212395336c9355c7e89e
pkhao75_Legion <132234282+pkhao75@users.noreply.github.com>
2024-07-23 19:21:38 -0400

    Update Model, implements getSource method

--- src/main/java/group5/model/IModel.java
+++ src/main/java/group5/model/IModel.java
@@ -8,17 +8,21 @@ import java.util.List;
 
 public interface IModel {
 
-    public static final String DEFAULT_DATA = "";  // Path to default movie DB here
+    public static final String DEFAULT_DATA = "./data/samples/source.json";  // Path to default movie DB here
 
     void loadSourceData();
 
-    void loadWatchList();
+    void loadWatchList(String filename);
 
     Stream<MBeans> getSourceLists();
 
-    Stream<MBeans> getWatchLists(String filename);
+    Stream<MBeans> getWatchLists(/*, UserListIdentifier userListId*/);
 
-    void saveWatchList(String filename, Stream<MBeans> watchList);
+    void saveWatchList(String filename, Stream<MBeans> watchList/*, UserListIdentifier userListId*/);
+
+    void updateWatched(MBeans media, boolean watched);
+
+    void updateUserRating(MBeans media, double rating);
 
     /*public Stream<MBeans> getMovieList(FilterClass filter) {
         return getMovieList(filter, null);
--- src/main/java/group5/model/Model.java
+++ src/main/java/group5/model/Model.java
@@ -2,31 +2,43 @@ package group5.model;
 
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import group5.model.beans.MBeans;
+import group5.model.formatters.MBeansLoader;
+import group5.model.formatters.Formats;
 
 public class Model implements IModel {
 
-    @Override
-    public void loadSourceData() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadSourceData'");
+    /** List of MBeans representing the source databse list. */
+    private List<MBeans> sourceList;
+
+    /** List of imdbID String to reference which media from source list is in watch list. */
+    private List<String> watchList;
+
+    public Model() {
+        loadSourceData();
     }
 
     @Override
-    public void loadWatchList() {
+    public void loadSourceData() {
+        System.out.println("Load source database from" + DEFAULT_DATA);
+        this.sourceList = MBeansLoader.loadMediasFromFile(DEFAULT_DATA, Formats.JSON);
+    }
+
+    @Override
+    public void loadWatchList(String filename) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'loadWatchList'");
     }
 
     @Override
     public Stream<MBeans> getSourceLists() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSourceLists'");
+        return this.sourceList.stream();
     }
 
     @Override
-    public Stream<MBeans> getWatchLists(String filename) {
+    public Stream<MBeans> getWatchLists() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getWatchLists'");
     }
@@ -37,12 +49,29 @@ public class Model implements IModel {
         throw new UnsupportedOperationException("Unimplemented method 'saveWatchList'");
     }
 
+    @Override
+    public void updateWatched(MBeans media, boolean watched) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateWatched'");
+    }
+
+    @Override
+    public void updateUserRating(MBeans media, double rating) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUserRating'");
+    }
+
 //    @Override
 //    public Stream<MBeans> getFiltered() {
 //        // TODO Auto-generated method stub
 //        throw new UnsupportedOperationException("Unimplemented method 'getFiltered'");
 //    }
 
-
-
+    /**
+     * Main method to test the model.
+     */
+    public static void main(String[] args) {
+        Model model = new Model();
+        System.out.println("Source list:" + model.getSourceLists().collect(Collectors.toList()));
+    }
 }
