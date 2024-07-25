63d5bd710b7348164cc25290c441c63a46c800db
pkhao75_Legion <132234282+pkhao75@users.noreply.github.com>
2024-07-25 15:44:51 -0400

    Move formatter methods to private and give only 1 public method

--- src/main/java/group5/model/formatters/MBeansFormatter.java
+++ src/main/java/group5/model/formatters/MBeansFormatter.java
@@ -74,25 +74,39 @@ public final class MBeansFormatter {
 //     }
 
 
-    public static void writeMediasToJSON(Collection<MBeans> records, OutputStream out) {
+    private static void writeMediasToJSON(Collection<MBeans> records, OutputStream out) {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         try {
             mapper.writeValue(out, records);
         } catch (IOException e) {
+            System.out.println("Error writing to JSON");
             e.printStackTrace();
         }
     }
 
-    public static void writeMediasToCSV(Collection<MBeans> records, OutputStream out) throws IOException {
+    private static void writeMediasToCSV(Collection<MBeans> records, OutputStream out) {
         CsvMapper mapper = new CsvMapper();
         CsvSchema schema = mapper.schemaFor(MBeans.class).withHeader();
         ObjectWriter csvWriter = mapper.writer(schema);
         try {
             csvWriter.writeValue(out, records);
         } catch (IOException e) {
-            System.out.println("the error is here");
+            System.out.println("Error writing to CSV");
             e.printStackTrace();
+        }
+    }
+
+    public static void writeMediasToFile(Collection<MBeans> records, OutputStream out, Formats format) {
+        switch (format) {
+            case JSON:
+                writeMediasToJSON(records, out);
+                break;
+            case CSV:
+                writeMediasToCSV(records, out);
+                break;
+            default:
+                System.out.println("Invalid format");
         }
     }
 
