31179ba84072e40921c05710d1e1a796c05d2ea8
Jack Einbinder <79175717+jackeinbinder01@users.noreply.github.com>
2024-07-25 19:05:49 -0400

    FilterPane resized

--- src/main/java/group5/view/FilterPane.java
+++ src/main/java/group5/view/FilterPane.java
@@ -91,7 +91,7 @@ public class FilterPane extends JPanel implements ActionListener, FocusListener 
         setComponentNames();
 
         // configure gbc
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.insets = new Insets(3, 2, 2, 2);
         updateGBC(null, null, null, null, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL);
 
         // add panels
