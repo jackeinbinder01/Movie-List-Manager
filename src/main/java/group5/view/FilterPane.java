7135f46e7516c8ed71b81f1fdc556216ef853d4c
Jack Einbinder <jackeinbinder01@gmail.com>
2024-07-31 16:07:29 -0400

    setMovies() revised

--- src/main/java/group5/view/FilterPane.java
+++ src/main/java/group5/view/FilterPane.java
@@ -287,6 +287,7 @@ public class FilterPane extends JPanel implements ActionListener, FocusListener 
             resetComboBoxOptions();
             refreshPlaceholders();
         }
+
     }
 
     /**
